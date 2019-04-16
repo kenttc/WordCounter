61e2e5257ac01ba74807d44a5ed61579404c906c
kenttc <kenttc@gmail.com>
2019-04-16 22:36:09 +0100

    Test case 4 . most degenerate test case i can think of.

--- WordCounterTests/WordCounterTests.cs
+++ WordCounterTests/WordCounterTests.cs
@@ -6,7 +6,10 @@ namespace WordCounterTests
     [TestClass]
     public class WordCounterTests
     {
-
+        /// <summary>
+        /// assumptions - words are spaced , 
+        /// and empty spaces are not counted. 
+        /// </summary>
 
 
         [TestMethod]
@@ -47,6 +50,17 @@ namespace WordCounterTests
             Assert.AreEqual(1, result[0].Count);
         }
 
-
+        [TestMethod]
+        public void Given_a_wordcounter_loaded_with_2_same_word_will_return_list_of_text_containing_1_word_and_count_of_2()
+        {
+            //arrange
+            var wordCounter = new WordCounter();
+            wordCounter.LoadText("sample sample");
+            //act
+            var result = wordCounter.GetTop10Words();
+            //assert
+            Assert.AreEqual("sample", result[0].Word);
+            Assert.AreEqual(2, result[0].Count);
+        }
     }
 }
