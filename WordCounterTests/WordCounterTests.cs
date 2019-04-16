a82b73d45e16bda09bac3ed95aafd97144550bda
kenttc <kenttc@gmail.com>
2019-04-16 22:24:42 +0100

    added test for 3rd case

--- WordCounterTests/WordCounterTests.cs
+++ WordCounterTests/WordCounterTests.cs
@@ -6,6 +6,9 @@ namespace WordCounterTests
     [TestClass]
     public class WordCounterTests
     {
+
+
+
         [TestMethod]
         public void Given_an_unloaded_wordcounter_will_return_list_of_empty_texts()
         {
@@ -24,13 +27,24 @@ namespace WordCounterTests
         {
             //arrange
             var wordCounter = new WordCounter();
-            var sampleText = "sample";
-            wordCounter.LoadText(sampleText);
+            wordCounter.LoadText("sample");
             //act
             var result = wordCounter.GetTop10Words();
             //assert
             Assert.AreEqual(1, result.Length);
+        }
 
+        [TestMethod]
+        public void Given_a_wordcounter_loaded_with_1_word_will_return_list_of_text_containing_1_word_and_count_of_1()
+        {
+            //arrange
+            var wordCounter = new WordCounter();
+            wordCounter.LoadText("sample");
+            //act
+            var result = wordCounter.GetTop10Words();
+            //assert
+            Assert.AreEqual("sample", result[0][0]);
+            Assert.AreEqual(12, result[0][1]);
         }
 
 
